b8594c373fb6c0ac4431f1c75c99b0291c6c8079
Alex Isayyid <6026917+aisayyid@users.noreply.github.com>
2020-07-05 18:28:21 -0400

    Set up all the prompts for generating readme.

--- index.js
+++ index.js
@@ -2,15 +2,55 @@ var inquirer = require("inquirer");
 var fs = require('fs');
 // array of questions for user
 inquirer.prompt([
-{
-    type: "input",
-    name: "title",
-    message: "What is your project title?"
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Give a description of the project."
+    },
+    {
+        type: "input",
+        name: "table of contents",
+        message: "What items would you like to make up your table of contents?"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process for this project."
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "How do you use the project?"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "Licensing.  What is the user allowed to do with the project?"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Would you like other users to contribute?  If so, explain how."
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Provide tests for users here, to demo the project and its use."
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "Put any questions here."
+    },
 
 
 
-}
-    ]);
+]);
 
 // function to write README file
 function writeToFile(fileName, data) {
